a666ce088dd4542a2232edbd78bd62e226c958a5
AdrianMedinaVidera <adrianmedinavidera@gmail.com>
2025-03-13 20:05:27 +0100

    responsive_header;

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import logo_eurovision from '../assets/logo_eurovision.png';
 import './header.css';
+import { useWindowSize } from '../api/helpers';
 
 type HeaderProps = {
     year: number,
@@ -16,38 +17,43 @@ type HeaderProps = {
 }
 
 const Header = ({year, arena, city, presenters, voting, slogan, logoUrl, country}: HeaderProps) => {
+    const { width } = useWindowSize();
+    
     return (
         <header>
             <div className='esc_title'>
                 <h1>Eurovision {year}</h1>
             </div>
             <div className='esc_description'>
-                <div className='image_container'>
-                    <div className='image_wrapper'>
-                        <img src={logoUrl ?? logo_eurovision} id='image'></img>
-                    </div>
-                </div>
-                <div id='esc_description_list'><h1 id='description'>Description: </h1>
-                <ul>
-                    <li>
-                        <div className='flex items-center gap-2'>
-                            <h3>Hosting Country:</h3>
-                            <span className='flex items-center gap-2'>
-                                {country.countryName}
-                                <img 
-                                    src={`https://flagcdn.com/20x15/${country.iso.toLocaleLowerCase()}.png`} 
-                                    alt={`Flag of ${country}`}
-                                    className='inline-block'
-                                />
-                            </span>
+                <div className={`content-wrapper ${width < 1200 ? 'flex-col' : 'flex-row'} flex items-center gap-4`}>
+                    <div className='image_container'>
+                        <div className='image_wrapper'>
+                            <img src={logoUrl ?? logo_eurovision} id='image' alt="Eurovision Logo"></img>
                         </div>
-                    </li>
-                    <li><h3>City: </h3>{city}</li> 
-                    <li><h3>Arena: </h3>{arena}</li>
-                    <li><h3>Presenters: </h3>{presenters.join(", ")}</li>
-                    <li><h3>Voting System: </h3>{voting}</li>
-                    <li><h3>Slogan: </h3>{slogan}</li>
-                </ul>
+                    </div>
+                    <div id='esc_description_list'>
+                        <h1 id='description'>Description: </h1>
+                        <ul>
+                            <li>
+                                <div className='flex items-center gap-2 flex-wrap'>
+                                    <h3>Hosting Country:</h3>
+                                    <span className='flex items-center gap-2'>
+                                        {country.countryName}
+                                        <img 
+                                            src={`https://flagcdn.com/20x15/${country.iso.toLocaleLowerCase()}.png`} 
+                                            alt={`Flag of ${country.countryName}`}
+                                            className='inline-block'
+                                        />
+                                    </span>
+                                </div>
+                            </li>
+                            <li><h3>City: </h3>{city}</li> 
+                            <li><h3>Arena: </h3>{arena}</li>
+                            <li><h3>Presenters: </h3>{presenters.join(", ")}</li>
+                            <li><h3>Voting System: </h3>{voting}</li>
+                            <li><h3>Slogan: </h3>{slogan}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
