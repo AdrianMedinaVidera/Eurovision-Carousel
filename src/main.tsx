17f4a7c8bdf9453211dffc0bed6900037373557f
AdrianMedinaVidera <adrianmedinavidera@gmail.com>
2025-03-18 16:47:55 +0100

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import ContestantDetails from './components/contestant-details.js'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/Eurovision-Carousel/">
+  <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/:year" element={<App />} />
-      </Route>
+      <Route path="/" element={<Navigate to="/2024" replace />} />
+      <Route path="/:year" element={<App />} />
       <Route
         path="/contestant/:year/:contestantId"
         element={<ContestantDetails />}
