68898e909f6ec772bb074aa50e403d22b49ff37f
Adrián Medina Videra <adrianmedinavidera@gmail.com>
2025-12-09 11:01:35 +0100

    Update API endpoints to use 'senior' path

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 export const obtenerDatosEdicion = async (year: number) => {
     try {
-        const response = await fetch(`https://eurovisionapi.runasp.net/api/contests/${year}`, {method: "GET"}); //Por defecto, fetch hace GET. || {method: "GET"}
+        const response = await fetch(`https://eurovisionapi.runasp.net/api/senior/contests/${year}`, {method: "GET"}); //Por defecto, fetch hace GET. || {method: "GET"}
         if (response.ok) {
             const datos = await response.json();
             return datos;
@@ -13,7 +13,7 @@ export const obtenerDatosEdicion = async (year: number) => {
 
 export const obtenerAños = async () => {
     try {
-        const response = await fetch(`https://eurovisionapi.runasp.net/api/contests/years`, {method: "GET"}); //Por defecto, fetch hace GET. || {method: "GET"}
+        const response = await fetch(`https://eurovisionapi.runasp.net/api/senior/contests/years`, {method: "GET"}); //Por defecto, fetch hace GET. || {method: "GET"}
         if (response.ok) {
             const datos = await response.json();
             return datos;
@@ -39,7 +39,7 @@ export const obtenerPaises = async () => {
 
 export const obtenerDatosConcursante = async (year: number, id: number) => {
     try {
-        const response = await fetch(`https://eurovisionapi.runasp.net/api/contests/${year}/contestants/${id}`, {method: "GET"});
+        const response = await fetch(`https://eurovisionapi.runasp.net/api/senior/contests/${year}/contestants/${id}`, {method: "GET"});
         if (response.ok) {
             const datos = await response.json();
             return datos;
